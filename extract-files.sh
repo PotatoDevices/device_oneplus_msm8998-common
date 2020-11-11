9ffc237d27caf0ee8abbf27a471049d98c925f31
Timi <timi.rautamaki@gmail.com>
2020-11-11 17:33:37 +0100

    msm8998-common: polish proprietary files list
     * Remove unused blobs
     * Remove duplicated blobs
     * Remove .rc files that are defined in init.*.rc in device tree
     * Combine all into proprietary-files.txt, removing QCPATH support
     * Alphabetize
    
    Change-Id: I6e95e5962e4a6dcfaf57c91e808431d9a0bd7cb7

--- extract-files.sh
+++ extract-files.sh
@@ -66,8 +66,6 @@ function blob_fixup() {
 # Initialize the helper
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$POTATO_ROOT" true "$CLEAN_VENDOR"
 
-extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
-extract "$MY_DIR"/proprietary-files-qc-perf.txt "$SRC" "$SECTION"
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
